c6c2dcfd2ea42af11ac716349c54a0b2e76b4c97
nunesluisa <lluisanunes01@gmail.com>
2026-09-10 18:51:40 +0000

    Cadastro inicial de pessoas

--- sistema-cadastro/main.py
+++ sistema-cadastro/main.py
@@ -1,1 +1,9 @@
 # Sistema de cadastro e gerenciamento
+print ('==========SISTEMA DE CADASTRO==========')
+print ('1 - Cadastrar a pessoa \n 2 - Listar cadastros \n 3 - Buscar cadastro \n 0 - Sair')
+opção = int (input ('Escolha uma opção: '))
+if opção == 1:
+    nome = input ('Digite o nome:')
+    idade = int(input ('Digite a idade: '))
+    cidade = input ('Qual a cidade: ')
+    print ('Os dados cadastrados são: \n', nome, '\n', idade, '\n', cidade, '\n' )
---
+++ strings.py
@@ -0,0 +1,3 @@
+frase = ('eu gosto de java')
+novafrase = frase.replace ('java', 'phyton')
+print (novafrase)
