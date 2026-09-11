34f9bf30f5430b45b9b2c5f3b8b066c76ac50622
nunesluisa <lluisanunes01@gmail.com>
2026-09-11 12:59:57 +0000

    Adição das opçoes

--- sistema-cadastro/main.py
+++ sistema-cadastro/main.py
@@ -1,9 +1,40 @@
 # Sistema de cadastro e gerenciamento
-print ('==========SISTEMA DE CADASTRO==========')
-print ('1 - Cadastrar a pessoa \n 2 - Listar cadastros \n 3 - Buscar cadastro \n 0 - Sair')
-opção = int (input ('Escolha uma opção: '))
-if opção == 1:
-    nome = input ('Digite o nome:')
-    idade = int(input ('Digite a idade: '))
-    cidade = input ('Qual a cidade: ')
-    print ('Os dados cadastrados são: \n', nome, '\n', idade, '\n', cidade, '\n' )
+pessoas = []
+while True:
+    print ('==========SISTEMA DE CADASTRO==========')
+    print ('1 - Cadastrar a pessoa \n 2 - Listar cadastros \n 3 - Buscar cadastro \n 0 - Sair')
+    opção = int (input ('Escolha uma opção: '))
+    if opção == 1:
+        nome = input ('Digite o nome:')
+        idade = int(input ('Digite a idade: '))
+        cidade = input ('Qual a cidade: ')
+        pessoa= {
+            'nome': nome,
+            'idade': idade,
+            'cidade': cidade
+        }
+        pessoas.append (pessoa)
+        print ('Os dados cadastrados são: \n', nome, '\n', idade, '\n', cidade, '\n' )
+    elif opção == 2:
+        print ('Acessando cadastro...')
+        if len (pessoas) == 0:
+            print ('Não tem pessoas cadastradas \nVoltando ao inicio')
+        else:
+            for pessoa in pessoas:
+                print (pessoa ['nome'])
+                print (pessoa ['idade'])
+                print (pessoa ['cidade'])
+                print ( '--------------------')
+    elif opção == 3:
+        busca = input ('Qual o nome você deseja buscar: ')
+        encontrou = False
+        for pessoa in pessoas:
+            if pessoa ['nome'] == busca.lower ():
+                print ('Achamos o cadastro correspondente', pessoa)
+                encontrou = True
+        if encontrou == False:
+            print('Não foram encontrados dados')
+    elif opção == 0:
+        print ('Você encerrou o cadastro!')
+        break 
+
--- strings.py
+++ strings.py
@@ -1,3 +1,15 @@
-frase = ('eu gosto de java')
-novafrase = frase.replace ('java', 'phyton')
-print (novafrase)
+pessoas = []
+continuar = "sim"
+while continuar == "sim":
+    nome = input('Digite o nome: ')
+    idade = int(input('Digite a idade: '))
+    cidade = input('Qual a cidade: ')
+
+    pessoa = { 'nome': nome,
+        'idade': idade,
+        'cidade': cidade
+    }
+    (pessoas.append(pessoa))
+    continuar = input ('você deseja continuar? Sim ou Não: ').strip().lower()
+print (pessoas)
+
